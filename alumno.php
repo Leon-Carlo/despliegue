b3b87018fc2fba4d37246c9c4a576f5c33ddbe6a
Leon-Carlo <carloraul826@gmail.com>
2025-03-05 11:22:49 -0500

    Create alumno.php

---
+++ alumno.php
@@ -0,0 +1,11 @@
+<?php
+
+$alumno = [
+"nombre" => "Carlo Raul",
+"apellido_paterno" => "Leon",
+"apellido_materno" => "Sandoval",
+"edad" => "21",
+"telefono" => "992778537",
+"email" => "XXX",
+];
+?>
